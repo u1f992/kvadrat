72aa304221f92286e3b7278af1cdc4ea829e0624
Koutaro Mukai <mukai.k1011k@outlook.jp>
2025-02-09 14:23:26 +0900

    Remove measureTime

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import path from "node:path";
 import { Worker } from "node:worker_threads";
 import { intToRGBA } from "jimp";
 import { ColorHex, colorHex } from "./color-hex.js";
-import { measureTime } from "./measure-time.js";
 
 type JimpImage = {
   width: number;
@@ -17,23 +16,21 @@ export async function toSVG(image: JimpImage) {
 
   // Mark all four edges of each square in clockwise drawing direction
   const edgesOf = new Map<ColorHex, [number, number, number, number][]>();
-  await measureTime("collectEdges", () => {
-    for (let x = 0; x < image.width; x++) {
-      for (let y = 0; y < image.height; y++) {
-        const hex = colorHex(intToRGBA(image.getPixelColor(x, y)));
-        if (!edgesOf.has(hex)) {
-          edgesOf.set(hex, []);
-        }
-        // prettier-ignore
-        edgesOf.get(hex)!.push(
-          [x    , y    , x + 1, y    ],
-          [x + 1, y    , x + 1, y + 1],
-          [x + 1, y + 1, x    , y + 1],
-          [x    , y + 1, x    , y    ],
-        )
+  for (let x = 0; x < image.width; x++) {
+    for (let y = 0; y < image.height; y++) {
+      const hex = colorHex(intToRGBA(image.getPixelColor(x, y)));
+      if (!edgesOf.has(hex)) {
+        edgesOf.set(hex, []);
       }
+      // prettier-ignore
+      edgesOf.get(hex)!.push(
+        [x    , y    , x + 1, y    ],
+        [x + 1, y    , x + 1, y + 1],
+        [x + 1, y + 1, x    , y + 1],
+        [x    , y + 1, x    , y    ],
+      )
     }
-  });
+  }
 
   // for (const [hex, edges] of edgesOf.entries()) {
   //   svg += toSVGPath(hex, edges);
--- src/measure-time.ts
+++
@@ -1,9 +0,0 @@
-export const measureTime = async (
-  label: string,
-  fn: () => any | Promise<any>,
-) => {
-  const start = process.hrtime.bigint();
-  const ret = await fn();
-  console.log(`${label}: ${(process.hrtime.bigint() - start) / 1000000n} ms`);
-  return ret;
-};
