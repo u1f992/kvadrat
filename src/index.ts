32182e5f282918405afd42e023fb184b50be4b4b
Koutaro Mukai <mukai.k1011k@outlook.jp>
2025-05-09 00:58:11 +0900

    Larger image for tracing, spike in processing time

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,76 @@
+import child_process from "node:child_process";
+
 import path from "node:path";
 import { Worker } from "node:worker_threads";
 import { intToRGBA } from "jimp";
+import * as jimp from "jimp";
 import { ColorHex, colorHex } from "./color-hex.js";
-
-type JimpImage = {
-  width: number;
-  height: number;
-  getPixelColor: (x: number, y: number) => number;
-};
 
 const WORKER = path.join(import.meta.dirname, "worker.js");
 
-export async function toSVG(image: JimpImage) {
-  let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${image.width}" height="${image.height}" viewBox="0 0 ${image.width} ${image.height}">`;
+function createPBM10x(
+  width: number,
+  height: number,
+  pixels: [number, number][],
+): string {
+  const w10 = width * 10;
+  const h10 = height * 10;
+  const map = new Set(pixels.map(([x, y]) => `${x},${y}`));
 
-  // Mark all four edges of each square in clockwise drawing direction
-  const edgesOf = new Map<ColorHex, [number, number, number, number][]>();
-  for (let x = 0; x < image.width; x++) {
-    for (let y = 0; y < image.height; y++) {
-      const hex = colorHex(intToRGBA(image.getPixelColor(x, y)));
-      if (!edgesOf.has(hex)) {
-        edgesOf.set(hex, []);
+  let pbm = `P1\n${w10} ${h10}\n`;
+  for (let y = 0; y < height; y++) {
+    for (let dy = 0; dy < 10; dy++) {
+      let row = "";
+      for (let x = 0; x < width; x++) {
+        const on = map.has(`${x},${y}`) ? "1" : "0";
+        row += `${on} `.repeat(10);
       }
-      // prettier-ignore
-      edgesOf.get(hex)!.push(
-        [x    , y    , x + 1, y    ],
-        [x + 1, y    , x + 1, y + 1],
-        [x + 1, y + 1, x    , y + 1],
-        [x    , y + 1, x    , y    ],
-      )
+      pbm += row + "\n";
     }
   }
+  return pbm;
+}
 
-  // for (const [hex, edges] of edgesOf.entries()) {
-  //   svg += toSVGPath(hex, edges);
-  // }
-  const promises: Promise<string>[] = [];
-  for (const [hex, edges] of edgesOf.entries()) {
-    promises.push(
-      new Promise((resolve, reject) => {
-        new Worker(WORKER, {
-          workerData: { hex, edges },
-        })
-          .on("message", resolve)
-          .on("error", reject);
-      }),
-    );
+export async function toSVG(image: Awaited<ReturnType<typeof jimp.Jimp.read>>) {
+  let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${image.width}" height="${image.height}" viewBox="0 0 ${image.width} ${image.height}">`;
+  console.log("start");
+
+  const width = image.width;
+  const height = image.height;
+  const colors = new Map<ColorHex, [number, number][]>();
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const rgba = intToRGBA(image.getPixelColor(x, y));
+      const hex = colorHex(rgba);
+      if (!colors.has(hex)) {
+        colors.set(hex, []);
+      }
+      colors.get(hex)!.push([x, y]);
+    }
   }
-  svg += (await Promise.all(promises)).join("");
+  console.log("done");
+
+  for (const [color, pixels] of colors.entries()) {
+    const pbm = createPBM10x(image.width, image.height, pixels);
+    const svg = new TextDecoder().decode(
+      child_process.spawnSync(
+        "potrace",
+        [
+          "-",
+          "--output",
+          "-",
+          "--svg",
+          "--turdsize=0",
+          "--alphamax=0",
+          "--longcurve",
+          "--resolution=720",
+          "--flat",
+        ],
+        { input: new TextEncoder().encode(pbm) },
+      ).stdout,
+    );
+    (await import("node:fs")).writeFileSync(`out/${color}.svg`, svg);
+  }
   svg += "</svg>";
   return svg;
 }
